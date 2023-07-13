82a3bfc15f2a213b58fdeeaca4cba4a16a8b5f5d
Forever Inc dev <120944776+foreverinc@users.noreply.github.com>
2023-07-13 10:05:29 +0300

    Update notification.js

--- js/notification.js
+++ js/notification.js
@@ -1,14 +1,25 @@
 const getPermissions = () => {
-	//default granted denied
-	if (Notification.permission === 'granted') {
-		//pass
-	} else if (Notification.permission !== 'denied') {
-		Notification.requestPermission().then(permission => {
-			//pass
-		})
+	if (Notification.permission === "granted") {
+		// Permission already granted
+	} else if (Notification.permission !== "denied") {
+		Notification.requestPermission().then((permission) => {
+			if (permission === "granted") {
+				showFirstNotification();
+			}
+		});
 	}
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
 	getPermissions();
 });
+
+const showFirstNotification = () => {
+	const notification = new Notification("Notification from Redo", {
+		body: "Hey friend, Redo app here",
+		icon: "https://i.ibb.co/YbFPdWS/redo-2-removebg-preview.png",
+	});
+	notification.onclick = (e) => {
+		window.location.href="/"
+	}
+};
